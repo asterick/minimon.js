d585771935bb411bcdf99f6a89f7a7d75143df93
Bryon Vandiver <unicdk@gmail.com>
2019-04-08 17:13:24 -0700

    Checkpoint

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,8 @@
 import { render } from 'inferno';
 import UI from  "./ui/index.jsx";
 
+console.log(require("./system/bios"));
+
 (async _ => {
 	render(<UI />, document.getElementById("container"));
-	start();
 })();
