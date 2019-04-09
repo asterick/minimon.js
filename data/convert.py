168b3e2e54993c5346e8fa7d192fb1114e49f6c0
Bryon Vandiver <unicdk@gmail.com>
2019-04-09 16:54:34 -0700

    Table work

--- data/convert.py
+++ data/convert.py
@@ -6,8 +6,93 @@ op0s, op1s, op2s = [None] * 0x100, [None] * 0x100, [None] * 0x100
 op0s[0xCE] = op1s
 op0s[0xCF] = op2s
 
+CONDITIONS = {
+    "LT": "LESS_THAN",
+    "LE": "LESS_EQUAL",
+    "GT": "GREATER_THAN",
+    "GE": "GREATER_EQUAL",
+    "V": "OVERFLOW",
+    "NV": "NOT_OVERFLOW",
+    "P": "POSITIVE",
+    "M": "MINUS",
+    "C": "CARRY",
+    "NC": "NOT_CARRY",
+    "Z": "ZERO",
+    "NZ": "NOT_ZERO",
+
+    "F0": "SPECIAL_FLAG_0",
+    "F1": "SPECIAL_FLAG_1",
+    "F2": "SPECIAL_FLAG_2",
+    "F3": "SPECIAL_FLAG_3",
+    "NF0": "NOT_SPECIAL_FLAG_0",
+    "NF1": "NOT_SPECIAL_FLAG_1",
+    "NF2": "NOT_SPECIAL_FLAG_2",
+    "NF3": "NOT_SPECIAL_FLAG_3"
+}
+
+ARGUMENTS = {
+    "ALL": "REGS_ALL",
+    "ALE": "REGS_ALE",
+
+    "A": "REG_A",
+    "B": "REG_B",
+    "L": "REG_L",
+    "H": "REG_H",
+
+    "BA": "REG_BA",
+    "HL": "REG_HL",
+    "IX": "REG_IX",
+    "IY": "REG_IY",
+
+    "NB": "REG_NB",
+    "BR": "REG_BR",
+    "EP": "REG_EP",
+    "IP": "REG_EP",
+    "XP": "REG_EP",
+    "YP": "REG_EP",
+    "SC": "REG_SC",
+
+    "SP": "REG_SP",
+    "PC": "REG_PC",
+
+    "[hhll]": "MEM_ABS16",
+    "[HL]": "MEM_HL",
+    "[IX]": "MEM_IX",
+    "[IY]": "MEM_IY",
+    "[SP+dd]": "MEM_SP_DISP",
+    "[IX+dd]": "MEM_IX_DISP",
+    "[IY+dd]": "MEM_IY_DISP",
+    "[IX+L]": "MEM_IX_OFF",
+    "[IY+L]": "MEM_IY_OFF",
+    "[BR:ll]": "MEM_BR",
+    "[kk]": "MEM_VECTOR",
+
+    "rr": "REL_8",
+    "qqrr": "REL_16",
+    "#nn": "IMM_8",
+    "#bb": "IMM_8",
+    "#pp": "IMM_8",
+    "#hh": "IMM_8",
+    "#mmnn": "IMM_16"
+
+}
+
 def format(op, arg1, arg2):
-	return { "op": op, "args": [arg for arg in arg1, arg2 if arg]}
+    try:
+        condition = None
+        if arg1 in CONDITIONS:
+            condition, arg1, arg2 = CONDITIONS[arg1], arg2, None
+
+    	return { 
+            "op": op, 
+            "condition": condition, 
+            "args": [ARGUMENTS[arg] for arg in arg1, arg2 if arg]
+        }
+    except Exception as e:
+        return e
+
+all_ops = []
+all_args = []
 
 with file('s1c88.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile)
@@ -24,5 +109,8 @@ with file('s1c88.csv', 'rb') as csvfile:
         	op1s[code] = format(op1, arg1_1, arg1_2)
         if op2 != 'undefined':
         	op2s[code] = format(op2, arg2_1, arg2_2)
+
+        all_ops += [op0, op1, op2]
+        all_args += [arg0_1, arg0_2, arg1_1, arg1_2, arg2_1, arg2_2]
 
 print dumps(op0s, indent=4)
