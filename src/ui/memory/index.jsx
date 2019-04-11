30d109a156cb0b29f479a97e828893c09c51f3d9
Bryon Vandiver <unicdk@gmail.com>
2019-04-11 16:39:47 -0700

    Fixed flickering

--- src/ui/memory/index.jsx
+++ src/ui/memory/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { AutoSizer, List } from 'react-virtualized';
 
-import style from "./style.css";
+import classes from "./style.css";
 
 const TOP_OF_MEMORY = 0x1FFFFF;
 
@@ -14,17 +14,17 @@ function toHex(i, c) {
 class MemoryCell extends Component {
 	render() {
 		return (
-			<span className={style['byte-cell']}>{toHex(this.props.value, 2)}</span>
+			<span className={classes['byte-cell']}>{toHex(this.props.value, 2)}</span>
 		);
 	}
 }
 
-function rowRenderer ({ key, index, parent }) {
+function rowRenderer ({ key, style, index, parent }) {
 	const { system, memoryBottom, bytesPerRow } = parent.props;
 
 	let address = index * bytesPerRow + memoryBottom;
 
-	const data = [<span className={style['address']} key={-1}>{toHex(address,6)}</span>]
+	const data = [<span className={classes['address']} key={-1}>{toHex(address,6)}</span>]
 
 	for (let i = 0; i < bytesPerRow; i++, address++) 
 		data.push(<MemoryCell key={address} value={system.read(address)} />);
@@ -39,9 +39,9 @@ function rowRenderer ({ key, index, parent }) {
 class MemoryEditor extends Component {
 	render() {
 		return (
-			<AutoSizer className={style.memory}>
+			<AutoSizer className={classes['memory']}>
 				{({ height, width }) => {
-					const bytesPerRow = 0x20;//Math.floor((width - 66 - 15) / 22) || 1;
+					const bytesPerRow = Math.floor((width - 66 - 15) / 22) || 1;
 
 					return (
 						<List
