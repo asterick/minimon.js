28a22e992aaa07b798389055a83b48b79402309b
Bryon Vandiver <unicdk@gmail.com>
2020-12-28 02:10:39 -0800

    Type annotation

--- src/s1c88.d.ts
+++ src/s1c88.d.ts
@@ -1,7 +1,21 @@
+interface UniformList {
+	[key: string]: WebGLUniformLocation;
+}
+
+interface AttributeList {
+	[key: string]: number;
+}
+
+interface Instruction {
+    op: string;
+    condition: number;
+    args: number[];
+}
+
 declare module "*.csv" {
     const Condition: {[key: string]: number};
     const Argument: {[key: string]: number};
-    const InstructionTable: any;
+    const InstructionTable: (Instruction | Instruction[] | null)[];
 }
 
 declare module 'raw-loader!*' {
